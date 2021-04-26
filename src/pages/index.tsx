f2e74ffa0bc2ac0f60ecc0ef2abb1c85a36e944c
vincentreynaud <vincentreynaud.de@gmail.com>
2021-04-26 17:24:33 +0200

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+// data
+
+// markup
+const IndexPage = () => {
+  return (>
+      <title>Home Page</title>
+      <h1>Hellooo</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
