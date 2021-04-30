b9306b68375f97d84a85d7ea7e193e743c960a0f
vincentreynaud <vincentreynaud.de@gmail.com>
2021-04-30 10:50:15 +0200

    add working css parallax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
+import "../styles/main.scss";
+import bgImg from "../images/pic9.jpg";
+import midImg from "../images/pic10.jpg";
+import forImg from "../images/pic8.jpg";
+
 
 const IndexPage = () => {
+
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Hellooo</h1>
+    <main className="parallax-container">
+      <div className="">
+        <img className="background__image" src={bgImg} />
+        <img className="middle__image" src={midImg} />
+        <img className="foreground__image" src={forImg} />
+      </div>
     </main>
   );
 };
